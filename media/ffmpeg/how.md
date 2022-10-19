605144554affab2e636b8e0785df55f442148d7c
kevindirect <git@kevindirect.com>
2022-10-18 20:08:51 -0700

    reorg

---
+++ media/ffmpeg/how.md
@@ -0,0 +1,3 @@
+
+# Trimming Video (-ss: start time HH:MM:SS, -to: end time HH:MM:SS)
+ffmpeg -ss 00:01:10 -to 00:02:10 -i <input> -c copy <output>
--- package-managers/conda/init_conda.sh
+++ package-managers/conda/init_conda.sh
@@ -5,27 +5,22 @@
 # This is a bash script because as of this writing conda only supports initialization for a small number of shells,
 # we can only activate a conda env on these supported shells, and not all of them are POSIX compliant.
 
-declare -a pkg_conda=("numpy" "pandas" "pyarrow" "arrow-cpp" "parquet-cpp" "numba" "scipy" "jupyter" "scikit-learn" "dask" "python-graphviz" "graphviz" "pygraphviz" "matplotlib" "statsmodels" "pymongo" "mongodb" "pytorch" "keras" "dill" "humanize");
-declare -a pkg_pip=();
+declare -a pkg_conda=("numpy" "pandas" "pyarrow" "scipy" "scikit-learn" "dask" "matplotlib" "tensorboard" "statsmodels" "jupyter" "humanize" "pytorch" "graphviz" "python-graphviz" "mongodb" "pymongo");
+declare -a pkg_pip=("pytorch-lightning" "duka" "kymatio" "more_itertools");
 
+#. "$HOME/miniconda3/etc/profile.d/conda.sh";
 alias car="conda activate" cdr="conda deactivate";
 car && trap cdr EXIT;
 conda update conda pip;
 
-# CONDA INSTALLS
-for i in "${pkg_conda[@]}"; do
-	yes | conda install "$i";
-done;
+conda install "${pkg_conda[@]}";
 
-## Alternate channel conda installations:
-# conda install -c conda-forge xonsh prompt_toolkit;
-# conda install -c conda-forge dask-ml;
-# conda install -c jaikumarm hyperopt;
-
-# PIP INSTALLS
 for i in "${pkg_pip[@]}"; do
 	yes | pip install -U "$i";
 done;
+
+## Alternate channel conda installations:
+# conda install -c jaikumarm hyperopt;
 
 ## Alternate pip installations:
 # pip install -U redis==2.10.6;
